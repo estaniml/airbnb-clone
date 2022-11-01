5d286cb179047a8df4b70e43a0802ce735883cca
Estanislao Martinez Lacabe <emartinezlacabe@gmail.com>
2022-11-01 16:58:14 -0300

    second commit

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+
+const Footer = () => {
+  return (
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-52 py-14 bg-gray-100 text-gray-600">
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+        </div>
+        
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accesibility</p>
+            <p>This is not a real site</p>
+            <p>Its just clone</p>
+            <p>Referral accepted</p>
+            <p>cf</p>
+        </div>
+        
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">CREATED WITH:</h5>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>Tailwind</p>
+            <p>React</p>
+            <p>Nextjs</p>
+        </div>
+        
+        <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>Investors</p>
+            <p>Wsp</p>
+            <p>247</p>
+            </div>
+
+    </footer>
+  )
+}
+
+export default Footer
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['links.papareact.com'],
+  },
   reactStrictMode: true,
   swcMinify: true,
+
 }
 
 module.exports = nextConfig
